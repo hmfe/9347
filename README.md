7452f5262e1e6a7e40fe30234a1fc54e3b0a409b
Henrik Holmlund <henrik.holmlund@digitalent.se>
2019-10-17 13:45:25 +0200

    remove gitignore files

--- README.md
+++ README.md
@@ -1,65 +1,68 @@
-# WORK TASK #
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-**Please read this document thoroughly** and plan your work accordingly.
-When ready, fork the repository and create a feature branch. It is in this branch we
-expect your delivery.
+## Available Scripts
 
-This Work Task is divided into three parts:
+In the project directory, you can run:
 
-### 1: CSS-wizardry ###
+### `yarn start`
 
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-![alt text](https://raw.githubusercontent.com/hmfe/54321/master/button.png)
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
 
-Recreate this button using only HTML/CSS and using as few HTML-elements as
-possible. (HINT: It is possible to solve this using only one `<button>`)
+### `yarn test`
 
+Launches the test runner in the interactive watch mode.<br />
+See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-* You can provide a solution for this separately or as a part of the solution for (3) *
+### `yarn build`
 
+Builds the app for production to the `build` folder.<br />
+It correctly bundles React in production mode and optimizes the build for the best performance.
 
-### 2: Please answer the questions found in the link below ###
-https://sv.surveymonkey.com/r/QCT8SHL
+The build is minified and the filenames include the hashes.<br />
+Your app is ready to be deployed!
 
-### 3: Implement a simple search application ###
+See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-<img src="https://raw.githubusercontent.com/hmfe/54321/master/search.png" style="width: 400px">
-Implement a simple search form. The search should use a public REST API of your
-choice using JavaScript.
+### `yarn eject`
 
-- Search for title, return title (or something like that)
-- Display partial search results in a list beneath the search field (Auto complete)
-- Display the selected results in an editable list beneath the search component 
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-  * Selected search result should be saved with date/time stamp (as a
-search history)
-  * User should be able to delete a result from the list or delete the entire
-list.
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-- The application should be responsive, adapting to changes of the viewport. 
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-### What we will look at: ###
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
 
-- HTML5: Semantic markup, SEO optimization, Accessibility
-   * Use HTML5 and show us that you have deep knowledge of semantic
-markup.
+## Learn More
 
-- CSS3: Responsivity, use of pseudo elements, HTML entities and complexity
-of solution
-  * Surprise us with interesting solutions and show off your skills!
-  * NOTE: Solution based on premade CSS frameworks will be completely discarded
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-- JS: complexity, sanity, comments and security.
-  * The search should fetch data for each entry, don’t store a complete
-database response in a variable and iterate through that.
-  * Sanitize your inputs and don’t stress the API more than necessary.
+To learn React, check out the [React documentation](https://reactjs.org/).
 
-- General sanity check on structure and solution
-  * Show to us that you know - how front-end development should be
-done.
+### Code Splitting
 
+This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
 
+### Analyzing the Bundle Size
 
-When done, push your branch and let us know it’s done by making a pull request.
+This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
 
-**Good Luck and have fun!**
+### Making a Progressive Web App
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+
+### Advanced Configuration
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+
+### Deployment
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
+
+### `yarn build` fails to minify
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
---
+++ src/App.js
@@ -0,0 +1,271 @@
+import React, { useState, useEffect } from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import axios from 'axios';
+// import debounce from 'lodash/debounce';
+import './App.css';
+import { device } from './styles/Device';
+
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const FavoriteList = styled.li`
+  background: rgba(255, 255, 255, 0.892);
+  list-style: none;
+  border-bottom: 1px solid #d8d8d8;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.14);
+  margin: 0;
+  padding: 20px;
+  border-radius: 4px;
+  transition: background 0.2s;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  text-transform: capitalize;
+  flex: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  font-family: Futura, 'Trebuchet MS', Arial, sans-serif;
+  @media ${device.laptop} {
+    width: 800px;
+    max-width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    text-transform: capitalize;
+  }
+`;
+const StyledH1 = styled.h1`
+  font-family: Futura, 'Trebuchet MS', Arial, sans-serif;
+`;
+
+const StyledH2 = styled.h2`
+  font-family: Futura, 'Trebuchet MS', Arial, sans-serif;
+`;
+
+const StyledInput = styled.input`
+  font: Futura, sans-serif;
+  width: 100%;
+  max-width: 200px;
+  box-sizing: border-box;
+  letter-spacing: 1px;
+  color: black;
+  margin: 0 20px;
+  &::focus {
+    outline: none;
+  }
+`;
+const DeleteButton = styled.button`
+  background: linear-gradient(90deg, #f4b05d, #f19639);
+  color: black;
+  font-weight: 900;
+  width: 135px;
+  height: 68px;
+  border: 1px solid #928574;
+  border-radius: 0;
+  font-size: 1rem;
+  display: inline-block;
+  transition: all 0.5s;
+  text-align: left;
+  vertical-align: middle;
+  position: relative;
+  &[disabled] {
+    opacity: 0.5;
+  }
+  &::after {
+    content: '\\00D7';
+    text-align: center;
+    height: 55px;
+    width: 55px;
+    background-color: white;
+    border-radius: 50%;
+    display: inline-block;
+    vertical-align: middle;
+    line-height: 48px;
+    text-align: center;
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    font-size: 40px;
+    font-weight: 500;
+    overflow: hidden;
+  }
+`;
+const AddButton = styled.button`
+  background: linear-gradient(90deg, #00d4ff, #2075c8);
+  color: black;
+  font-weight: 900;
+  width: 135px;
+  height: 68px;
+  border: 1px solid #928574;
+  border-radius: 0;
+  font-size: 1rem;
+  display: inline-block;
+  transition: all 0.5s;
+  text-align: left;
+  vertical-align: middle;
+  position: relative;
+  &[disabled] {
+    opacity: 0.5;
+  }
+  &::after {
+    content: '\\2713';
+    text-align: center;
+    height: 55px;
+    width: 55px;
+    background-color: white;
+    border-radius: 50%;
+    display: inline-block;
+    vertical-align: middle;
+    line-height: 52px;
+    text-align: center;
+    position: absolute;
+    right: 5px;
+    top: 5px;
+    font-size: 40px;
+    font-weight: 500;
+    overflow: hidden;
+  }
+`;
+
+const Page = styled.div`
+  background-color: white;
+  height: 100vh;
+  width: 100%;
+  margin: 0 auto;
+
+  @media ${device.laptop} {
+    width: 100%;
+    max-width: 800px;
+  }
+`;
+function App() {
+  const [list, setList] = useState([]);
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('');
+  const [visible, setVisible] = useState(false);
+  const [singular, setSingular] = useState(false);
+
+  function removeFavorite(id) {
+    const newList = list.filter(item => item.objectID !== id);
+    setList(newList);
+  }
+  function addFavorite(id) {
+    const dateAdded = new Date(Date.now()).toLocaleString('sv-SE');
+    const newList = data.hits.filter(item => item.objectID === id);
+    const newObject = newList[0];
+    newObject.dateAdded = dateAdded;
+
+    const duplicateExists = list.filter(
+      item => item.objectID === newObject.objectID
+    );
+
+    if (duplicateExists.length === 0) {
+      setList(list => [...list, newObject]);
+      setSingular(true);
+      setVisible(true);
+      setTimeout(() => {
+        setVisible(false);
+      }, 1200);
+    } else {
+      setSingular(false);
+      setVisible(true);
+      setTimeout(() => {
+        setVisible(false);
+      }, 1200);
+    }
+  }
+  useEffect(() => {
+    if (query.length === 0) {
+      setData({ hits: [] });
+    }
+    if (query.length < 3) return;
+    const getDataFromAPI = async () => {
+      // console.log('loading...', query);
+      const result = await axios(
+        `http://hn.algolia.com/api/v1/search?query=${query}`
+      );
+      setData(result.data);
+    };
+    getDataFromAPI();
+  }, [query]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Page className='App'>
+        <StyledH1>This is the awesome test page</StyledH1>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column'
+          }}
+        >
+          <StyledH2>Add your favorite article, h4xxor</StyledH2>
+
+          <StyledInput
+            placeholder='Search for something'
+            type='text'
+            value={query}
+            onChange={event => setQuery(event.target.value)}
+          />
+          <div style={{ height: '20px', margin: '5px' }}>
+            {visible === true && (
+              <span>
+                {singular ? `Added to favorite!` : `Favorite already added...`}
+              </span>
+            )}
+          </div>
+
+          <ul style={{ padding: 0 }}>
+            {data.hits.map(item => (
+              <FavoriteList key={item.objectID} id={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+                <AddButton onClick={() => addFavorite(item.objectID)}>
+                  Add
+                </AddButton>
+              </FavoriteList>
+            ))}
+          </ul>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            flexDirection: 'column'
+          }}
+        >
+          {list.length > 0 && (
+            <StyledH2>
+              I currently have {list.length} favorites, soo exciting!
+            </StyledH2>
+          )}
+
+          <ul id='favorites' style={{ padding: 0 }}>
+            {list.map(item => (
+              <FavoriteList key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+                <span>Added on: {item.dateAdded}</span>
+                <DeleteButton
+                  type='button'
+                  onClick={() => removeFavorite(item.objectID)}
+                >
+                  Delete
+                </DeleteButton>
+              </FavoriteList>
+            ))}
+          </ul>
+        </div>
+      </Page>
+    </ThemeProvider>
+  );
+}
+
+export default App;
